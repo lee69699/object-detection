8a547f65f57419a88235f1eb9fb0326cd5c4692e
lee69699 <86499411+lee69699@users.noreply.github.com>
2021-06-26 00:38:13 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,49 @@
+
+from flask import Flask, render_template, request, flash, redirect
+from werkzeug.utils import secure_filename
+from model import search_frames, video_to_frames
+import os
+import json
+
+UPLOAD_FOLDER = 'uploads/'
+
+app = Flask(__name__)
+app.config['SESSION_TYPE'] = 'memcached'
+app.config['SECRET_KEY'] = 'super secret key'
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+
+@app.route('/', methods=["GET"])
+def index():
+    with open('objects.txt', 'r') as f:
+        obj_nam = set(json.loads(f.read()))
+    return render_template('index.html', obj_names=obj_nam)
+
+@app.route('/', methods=["POST"])
+def upload():
+    if request.method == 'POST':
+        file = request.files['videoupload']
+        filename = secure_filename(file.filename)
+        #os.mkdir('uploads', 777)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        video_to_frames(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        #os.remove("uploads")
+        flash('File(s) successfully uploaded')
+        return redirect('/')
+    
+
+        
+           
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+   
+    search_text = request.form.get("search_parameter")
+    objects = search_frames(search_text)
+    #video = os.listdir('static/video_frames')
+    return render_template('search.html', frames_list=objects, search_txt=search_text)
+		
+if __name__ == '__main__':
+    app.run(host='localhost', port=3000, debug=True)
+
---
+++ model.py
@@ -0,0 +1,66 @@
+from  keras.models import load_model
+from keras.applications.vgg16 import  preprocess_input, decode_predictions, VGG16
+from keras.preprocessing.image import load_img, img_to_array
+import numpy as np
+from glob import glob
+import cv2
+import os
+import json
+
+VGG16_Model = VGG16(weights='imagenet', include_top=True, input_shape=None, input_tensor=None)
+VIDEO_FRAMES = glob("static/video_frames/*.jpg")
+
+distinct_objects = set()
+
+def video_to_frames(video_file):
+    vidcap = cv2.VideoCapture(video_file)
+    success,image = vidcap.read()
+    count = 0
+    while(vidcap.isOpened()):
+        success, frame = vidcap.read()
+        if success:
+            cv2.imwrite('static/video_frames/frame'+str(count)+'.jpg', frame)
+        else:
+             break
+        count = count+1
+        
+    detected_objects = feeding_frames_to_vgg16(VIDEO_FRAMES)
+    with open('objects.txt', 'w') as f:
+        f.write(json.dumps(detected_objects))
+    vidcap.release()
+    cv2.destroyAllWindows()
+
+  
+
+def feeding_frames_to_vgg16(frame_list):
+    features = []
+    for item in frame_list:
+        image = load_img(item, target_size=(224, 224))
+        image = img_to_array(image)
+        image = np.expand_dims(image, axis=0)
+        image = preprocess_input(image)
+        y_pred = VGG16_Model.predict(image)
+        label = decode_predictions(y_pred)
+        features.append(label[0][1][1])
+        #distinct_objects.add(label[0][1][1])
+    return features
+
+def search_frames(obj):
+    with open('objects.txt', 'r') as f:
+        detected_objects = json.loads(f.read())
+    result_list = []
+    indexes = []
+    if obj in set(detected_objects):
+      for i in range(len(detected_objects)):
+          if obj.__eq__(detected_objects[i]):
+              img_url = VIDEO_FRAMES[i]
+              path_arr = img_url.split('\\')
+              img_url = os.path.join('video_frames/', path_arr[1])
+              result_list.append(img_url)
+              indexes.append(i)
+    else:
+        return 'Object does not exist' 
+    return result_list 
+
+        
+        
